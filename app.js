787f1a68c9c02e0b56932a9e5b4c9b4baf5717ee
Samylli Zhao <winie1022@gmail.com>
2025-09-22 16:14:58 -0700

    novas funcoes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let amigos = []; //lista de amigos
+let amigos = [];     //lista de amigos
+let sorteados = [];  // lista de sortados
 
 function adicionarAmigo(nomeAmigo){
     nomeAmigo = document.querySelector('input').value;
@@ -6,44 +7,48 @@ function adicionarAmigo(nomeAmigo){
     if (nomeAmigo == ''){
         alert('Por favor, insira um nome.');
     } else {
-        if (amigos.includes(nomeAmigo)){
-            alert("Esse nome já existe na lista.");
+        if (amigos.includes(nomeAmigo)){                  //verificar se o nome ja esta na lista
+            alert("Esse nome já existe na lista."); 
         }else {         
-            amigos.push(nomeAmigo); //adiciona o nome a lista >amigos<
-            document.querySelector('input').value = ''; //esvazia lista
-            atualizarListaDeAmigos(); //chama carregar lista 
+            amigos.push(nomeAmigo);                       //adiciona o nome a lista "amigos"
+            document.querySelector('input').value = '';   //esvazia a lista 
+            atualizarListaDeAmigos();                     //carregar lista 
         }
     } 
 }
 
 function atualizarListaDeAmigos(){
     let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = ''; //limpa lista
+    lista.innerHTML = ''; 
 
-    for (let i = 0; i < amigos.length; i++){ // iniciacao -> condicao -> incremento entao cria variavel, se i e menor que tam da lista, incrementa.
-        let li = document.createElement('li');
+    for (let i = 0; i < amigos.length; i++){ 
+        let li = document.createElement('li');             //cria um elemento no html chamdo li 
         li.textContent = amigos[i];
-        lista.appendChild(li); 
+        lista.appendChild(li);                             //adiciona a lista ao li 
     }
 }
 
 function sortearAmigo(){
-    if( amigos.length === 0 ){
-        alert("Por favor, insira um nome.");
-    } else{
-        let indice = Math.floor(Math.random() * amigos.length);
-        let nomeSorteado = amigos[indice];
-        document.getElementById('resultado').innerHTML = nomeSorteado
+    if (sorteados.length === amigos.length) {
+    document.getElementById('resultado').innerHTML = "Todos os amigos já foram sorteados!";
+    return;
     }
+
+    let indice = Math.floor(Math.random() * amigos.length);
+
+    while(sorteados.includes(indice)){
+        indice = Math.floor(Math.random() * amigos.length);
+    }
+    let nomeSorteado = amigos[indice];
+
+    sorteados.push(indice);
+    document.getElementById('resultado').innerHTML = nomeSorteado;   
 }
 
 function limparLista(){
-    if(amigos.length){
-        amigos = []
-        document.getElementById('listaAmigos').innerHTML = ''; // limpa a lista na tela
-        document.getElementById('resultado').innerHTML = ''; // limpa o resultado sorteado
-        atualizarListaDeAmigos()
-    }else{
-        alert('Nada para limpar.')
-    }
+    amigos = [];
+    sorteados = [];
+    document.getElementById('listaAmigos').innerHTML = ''; // limpa a lista na tela
+    document.getElementById('resultado').innerHTML = '';   // limpa o resultado sorteado
+    atualizarListaDeAmigos();
 }
